8781cf56749b72dff1f71530702409f16d8e3862
Savio Gabriel Santos <santos.savio@adventistas.org>
2026-02-14 22:50:47 -0300

    Desabilitado debug

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
     
     print("Servidor Flask iniciado!")
     print("Acesse: http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
